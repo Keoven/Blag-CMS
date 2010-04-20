369e3ee10494eeefc2b762cf45efc561a123414a
G2iX <g2ix@g2ix-laptop.(none)>
2010-04-20 18:50:56 +0800

    Fix Author Creation v2

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,5 @@
 class AuthorsController < ApplicationController
-  before_filter :require_author, :except => [:new, :create]
+  before_filter :require_author, :except => [:new, :create, :index]
 
   def index
     @authors = Author.all
