349c26d577b10c4a386af99c838fc64c17f3d0f2
G2iX <g2ix@g2ix-laptop.(none)>
2010-05-06 10:24:27 +0800

    Created Helper for Generating List of Articles in Index of Articles

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,4 +1,17 @@
 module ArticlesHelper
+
+  #Returns HTML code for articles
+  #
+  def retrieve_articles(articles)
+    str = String.new
+    articles.each_with_index do |article, i|
+      str << "<tr class='#{ i % 2 == 1 ? 'rowodd' : 'roweven' }'>"
+      str << format_article(article)
+      str << "</tr>"
+    end
+    return str
+  end
+
   #Returns HTML code for comments of an article
   #
   def retrieve_article_comments(article)
@@ -16,6 +29,20 @@ module ArticlesHelper
   end
 
   private
+  #Returns HTML formatted article
+  #
+  def format_article(article)
+    %Q{ <td>#{truncate(article.title, :length => 10)}</td>
+        <td>#{truncate(article.body, :length => 10)}</td>
+        <td>#{article.status}</td>
+        <td>#{full_name(Author.find(article.author_id))}</td>
+        <td>#{formatted_date_posted(article)}</td>
+        <td class="article-index-entry">#{link_to 'Edit', edit_article_path(article), :class => 'offlink'}</td>
+        <td class="article-index-entry">#{link_to 'Show', article_path(article), :class => 'offlink'}</td>
+        <td class="article-index-entry">#{link_to 'Delete', article, :confirm => "Are you sure you wane to delete article \"#{article.title}\"?",
+                                                                     :method => :delete,
+                                                                     :class => 'offlink'}</td> }
+  end
   #Returns HTML formatted comment
   #
   def format_article_comment(comment)
