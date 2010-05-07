4b22d1f2225d77db0b58374f50fc6dd8798d06da
G2iX <g2ix@g2ix-laptop.(none)>
2010-05-07 15:19:03 +0800

    Setting Up Capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,29 @@
+set :application, "Blag"
+set :repository,  "git@github.com:Keoven/Blag-CMS.git"
+
+set :scm, :git
+
+role :web, "175.41.165.67"
+role :app, "175.41.165.67"
+role :db,  "175.41.165.67", :primary => true
+#role :db,  "your slave db-server here"
+
+set :use_sudo, false
+set :deploy_to, "mnt/webapps/#{application}"
+
+namespace :deploy do
+
+  desc "Restart the mongrel cluster"
+  task :restart do
+    run "/ecclabs/ecc_restart_app.sh #{application}"
+  end
+
+  desc "Setup application via simple script"
+  task :config_app do
+    run "/ecclabs/ecc_create_app.rb #{application}"
+  end    
+          
+  #hooks 
+  after "deploy:setup", "deploy:config_app"
+
+end
